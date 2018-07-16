ac619fbb89e447db5749566e465bfdc88b1255bf
Zabdiel García <zabadiel071@gmail.com>
2018-07-15 23:07:54 -0500

    Load syntax table from file in JSON.

--- src/main/kotlin/parser/AnalysisTable.kt
+++ src/main/kotlin/parser/AnalysisTable.kt
@@ -3,21 +3,50 @@ package parser
 import com.google.gson.Gson
 import java.io.File
 
-data class EntryData(val terminal:String, val noTerminals:ArrayList<String>)
-
-data class TableEntry(val noTerminal:String, val productions:ArrayList<EntryData>)
-
-fun analysisTable(): HashMap<String, ArrayList<EntryData>> {
-    val jsonString = File("analysisTable.json").readText()
-
-    val gson = Gson()
-    val elements = gson.fromJson(jsonString, Array<TableEntry>::class.java)
-
-    val map = HashMap<String,ArrayList<EntryData>>()
-
-    elements.forEach { tableEntry ->
-        map.put(tableEntry.noTerminal,tableEntry.productions)
+data class EntryData(val terminal:String,
+                     val symbols:ArrayList<String>
+){
+    override fun toString(): String {
+        return "(terminal=$terminal, symbols=$symbols)"
     }
+}
 
-    return map
+data class TableEntry(val noTerminal:String, val productions:ArrayList<EntryData>, val sync:ArrayList<String>)
+
+
+
+class AnalysisTable{
+    companion object {
+        val terminals = listOf<String>("id","+","-","*","/","(",")")
+
+        fun analysisTable(): HashMap<String, ArrayList<EntryData>> {
+            val jsonString = File("analysisTable.json").readText()
+
+            val gson = Gson()
+            val elements = gson.fromJson(jsonString, Array<TableEntry>::class.java)
+
+            val map = HashMap<String,ArrayList<EntryData>>()
+
+            elements.forEach { tableEntry ->
+                map.put(tableEntry.noTerminal,tableEntry.productions)
+            }
+
+            return map
+        }
+
+        fun syncList(): HashMap<String,ArrayList<String>>{
+            val jsonString = File("analysisTable.json").readText()
+
+            val gson = Gson()
+            val elements = gson.fromJson(jsonString, Array<TableEntry>::class.java)
+
+            val map = HashMap<String,ArrayList<String>>()
+
+            elements.forEach { tableEntry ->
+                map.put(tableEntry.noTerminal,tableEntry.sync)
+            }
+
+            return map
+        }
+    }
 }
---
+++ src/main/kotlin/parser/Parser.kt
@@ -0,0 +1,106 @@
+package parser
+
+import java.util.*
+
+class Parser(val tokenList:Queue<String>) {
+
+    val analysisTable = AnalysisTable.analysisTable()
+    val syncList = AnalysisTable.syncList()
+    val stack = Stack<String>()
+    val trace:Queue<String> = LinkedList<String>()
+
+    /**
+     *  Following the algorithm in the book
+     */
+    fun syntaxAnalysis(){
+        var token = tokenList.poll()
+        stack.push("$")
+        stack.push("E")
+        loop@ while (!stack.empty()){
+            val productions = analysisTable[stack.peek()]       //All the productions for the No Terminal given
+            when{
+                stack.peek() == token -> {
+                    val pop = stack.pop()
+                    token = tokenList.poll()
+                    event(pop)
+                }
+                isTerminal(stack.peek()) -> {
+                    parseError(token)
+                    break@loop
+                }
+                isErrorEntry(token, productions) -> {
+                    parseError(token)
+                    if (canSync(token,stack.peek()))
+                        token = tokenList.poll()
+                    else
+                        break@loop
+                }
+                else -> {
+                    var symbolsTail:ArrayList<String> = ArrayList()
+                    productions!!.forEach { entryData: EntryData ->
+                        if (entryData.terminal == token)
+                            symbolsTail = entryData.symbols
+                    }
+                    event(stack.pop(),symbolsTail)
+                    (symbolsTail.size-1 downTo 0)
+                            .filter { symbolsTail[it] != "" }
+                            .forEach { stack.push(symbolsTail[it]) }
+                }
+            }
+        }
+    }
+
+    /**
+     * Checks if is possible to sync an error
+     */
+    private fun canSync(token: String?, symbol:String): Boolean {
+        val syncItems = syncList[symbol]
+        return if (syncItems!=null)
+            syncItems!!.contains(token)
+        else
+            false
+    }
+
+    /**
+     *
+     */
+    private fun parseError(s:String){
+        trace.add("Unexpected token $s")
+    }
+
+    /**
+     *
+     */
+    private fun event(s:String){
+        trace.add("Match $s")
+    }
+
+    /**
+     *
+     */
+    private fun event(s: String?, symbols: ArrayList<String>) {
+        var production =""
+        symbols.forEach { s: String ->
+            production += s
+        }
+        trace.add("$s -> $production")
+    }
+
+    /**
+     *
+     */
+    private fun isErrorEntry(s: String?,productions: ArrayList<EntryData>?): Boolean {
+        return productions!!.none { entryData: EntryData ->
+            s == entryData.terminal
+        }
+    }
+
+    /**
+     * 
+     */
+    private fun isTerminal(s: String): Boolean {
+        return AnalysisTable.terminals.contains(s)
+    }
+
+
+}
